def5b7dce5a7d16c9443a93c3646a347b47a21ec
BabySid <blackfox1983@163.com>
2022-04-02 17:32:25 +0800

    more contains

--- contains.go
+++ contains.go
@@ -9,3 +9,23 @@ func ContainsString(array []string, val string) int {
 
 	return -1
 }
+
+func ContainsInterface(array []interface{}, val interface{}) int {
+	for i, item := range array {
+		if item == val {
+			return i
+		}
+	}
+
+	return -1
+}
+
+func ContainsInt64(array []int64, val int64) int {
+	for i, item := range array {
+		if item == val {
+			return i
+		}
+	}
+
+	return -1
+}
