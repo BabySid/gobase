44ad395e7fa75ecefaa1340038f5342c223003b5
BabySid <blackfox1983@163.com>
2022-09-09 15:38:08 +0800

    add signal

---
+++ signal.go
@@ -0,0 +1,33 @@
+package gobase
+
+import (
+	"os"
+	"os/signal"
+	"sync"
+)
+
+type SignalHandler func(sig os.Signal)
+
+type SignalSet struct {
+	handles sync.Map
+}
+
+func NewSignalHandles() *SignalSet {
+	s := &SignalSet{handles: sync.Map{}}
+	go s.run()
+	return s
+}
+
+func (s *SignalSet) Register(sig os.Signal, handler SignalHandler) {
+	s.handles.Store(sig, handler)
+}
+
+func (s *SignalSet) run() {
+	c := make(chan os.Signal)
+	signal.Notify(c)
+	for sig := range c {
+		if h, ok := s.handles.Load(sig); ok {
+			h.(SignalHandler)(sig)
+		}
+	}
+}
