e2c24714c27fda8447e8c308391e25d4a0267527
BabySid <blackfox1983@163.com>
2022-09-26 11:38:15 +0800

    add hostip

--- host_info.go
+++ host_info.go
@@ -5,6 +5,7 @@ import (
 	"github.com/shirou/gopsutil/v3/disk"
 	"github.com/shirou/gopsutil/v3/host"
 	"github.com/shirou/gopsutil/v3/mem"
+	"net"
 	"time"
 )
 
@@ -31,4 +32,30 @@ func GetDiskPartitions() []string {
 	}
 
 	return rs
+}
+
+func GetLocalIP() (string, error) {
+	iFaces, e := net.Interfaces()
+	if e != nil {
+		return "", e
+	}
+
+	for _, face := range iFaces {
+		addrs, err := face.Addrs()
+		if err != nil {
+			continue
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+				if ip.IsGlobalUnicast() {
+					return ip.String(), nil
+				}
+			}
+		}
+	}
+
+	return "", nil
 }
