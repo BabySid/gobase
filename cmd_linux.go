d2fb810a78e010a90d8b0dcd7014007a701de373
BabySid <blackfox1983@163.com>
2022-10-08 11:54:01 +0800

    add cmd_linux

---
+++ cmd_linux.go
@@ -0,0 +1,15 @@
+package gobase
+
+import "os/exec"
+
+func SetChildrenProcessDetached(c *exec.Cmd) {
+	c.SysProcAttr = &syscall.SysProcAttr{}
+}
+
+func SetChildrenProcessGroupID(c *exec.Cmd) {
+	if c.SysProcAttr == nil {
+		c.SysProcAttr = &syscall.SysProcAttr{}
+	}
+	c.SysProcAttr.Setpgid = true
+	c.SysProcAttr.Pdeathsig = syscall.SIGTERM
+}
