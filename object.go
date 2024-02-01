ea2347ce110397ef07507502d8f28914d110e737
BabySid <blackfox1983@163.com>
2024-02-01 16:17:07 +0800

    copy field

--- object.go
+++ object.go
@@ -11,3 +11,17 @@ func GetFullNameOfPointer(i any) string {
 	True(reflect.TypeOf(i).Kind() == reflect.Pointer)
 	return reflect.TypeOf(i).Elem().String()
 }
+
+func CopyField(dst any, src any, fields ...string) {
+	dObj := reflect.ValueOf(dst).Elem()
+	sObj := reflect.ValueOf(src).Elem()
+
+	for _, item := range fields {
+		dField := dObj.FieldByName(item)
+		sField := sObj.FieldByName(item)
+
+		TrueF(dField.IsValid() && sField.IsValid(), "invalid field: %s", item)
+
+		dField.Set(sField.FieldByName(item))
+	}
+}
