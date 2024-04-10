9fceb569daa5b1ec1a9599f2a833b6672104003d
BabySid <blackfox1983@163.com>
2024-04-10 11:26:05 +0800

    add caller

--- runtime.go
+++ runtime.go
@@ -1,12 +1,15 @@
 package gobase
 
 import (
+	"path/filepath"
 	"runtime"
 	"strings"
 )
 
 var (
-	CurCaller = 1
+	CurCaller         = 1
+	DefaultSkipCaller = 2
+	DefaultMaxCaller  = 15
 )
 
 func GetFileInfoOfCaller(skip int) (string, string, int) {
@@ -21,4 +24,30 @@ func GetFileInfoOfCaller(skip int) (string, string, int) {
 func GetShortFuncName(long string) string {
 	arr := strings.Split(long, ".")
 	return arr[len(arr)-1]
+}
+
+type CallFrame struct {
+	Line     int
+	Function string
+	File     string
+}
+
+func GetCallerFrames(max int, skip int, fullPath bool) []CallFrame {
+	var frames []CallFrame
+	pcs := make([]uintptr, max)
+	depth := runtime.Callers(skip, pcs)
+	fs := runtime.CallersFrames(pcs[:depth])
+
+	for f, again := fs.Next(); again; f, again = fs.Next() {
+		cf := CallFrame{
+			Line:     f.Line,
+			Function: f.Function,
+			File:     f.File,
+		}
+		if !fullPath {
+			cf.File = filepath.Base(cf.File)
+		}
+		frames = append(frames, cf)
+	}
+	return frames
 }
